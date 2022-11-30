5e31f99e7c89c59c2b764cd491125c3206435dc1
Francisco Castellano <francisco.castellano@zolva.es>
2022-11-30 07:50:33 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/cards2study/cards2study/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cards2study.cards2study
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:cards2study/pages/loading_page.dart';
+import 'package:cards2study/pages/registration_page.dart';
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const HomePage(),
+      '/loading': (context) => const LoadingPage(),
+      '/registration': (context) => const RegistrationPage(),
+    },
+  ));
